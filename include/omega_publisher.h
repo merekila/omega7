6e9aa043ff2facb5c37f1a627b3448c73e8e4e57
medlab <tututu@tut.tut-darmstut.tut>
2018-07-25 14:25:54 +0200

    implemented another version of transformation to avoid a direct RPY input for computing the quaternion

--- include/omega_publisher.h
+++ include/omega_publisher.h
@@ -3,9 +3,13 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
 #include <stdio.h>
+#include <math.h>
 #include "dhdc.h"
 #include "tf/tf.h"
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/LinearMath/Transform.h>
 
 #include "std_msgs/Int64.h"
 #include <sstream>
@@ -19,7 +23,7 @@ public:
     CE_OO(ros::NodeHandle*);
     bool run();
     void setScalingFactors(double scalefactor_trans_x, double scalefactor_trans_y, double scalefactor_trans_z, double scalefactor_rot_x, double scalefactor_rot_y, double scalefactor_rot_z);
-
+    void computeQuaternionFromSpatialRotation(double x_rot, double y_rot, double z_rot, tf::Quaternion quat);
 protected:
     ros::NodeHandle *n;
     ros::Publisher omega_pub;
@@ -35,8 +39,15 @@ protected:
     double ox, oy, oz;
     double scaled_ox, scaled_oy, scaled_oz;
     tf::Quaternion q;
+    tf::Quaternion q_raw;
+    //tf::Matrix3x3 rotMatrix;
+    geometry_msgs::Quaternion q_msg;
     double gr;
     double scale_trans_x, scale_trans_y, scale_trans_z;
     double scale_rot_x, scale_rot_y, scale_rot_z;
+    double euler_x_omega, euler_y_omega, euler_z_omega;
+    //new euler variables for adjusting the coordinate frame of omega to the endeffector frame
+    double euler_x_omega_new, euler_y_omega_new, euler_z_omega_new;
+    tf::Matrix3x3 rotMatrix, rollMatrix, pitchMatrix, yawMatrix;
 };
 #endif
--- src/omega7_qt.cpp
+++ src/omega7_qt.cpp
@@ -53,10 +53,18 @@ void CE_OO::setScalingFactors(double scalefactor_trans_x, double scalefactor_tra
     scale_rot_y = scalefactor_rot_y;
     scale_rot_z = scalefactor_rot_z;
 }
+/*not completed...
+    void computeQuaternionFromSpatialRotation(double x_rot, double y_rot, double z_rot, tf::Quaternion quat){
+        quat.x()= cos(x_rot/2);
+        quat.y()= sin(x_rot/2)
+        quat.z()= sin(x_rot/2)
+        quat.w()= sin(x_rot/2)
+    }
+*/
 
 bool CE_OO::run(){
 
-    ros::Rate r(50);
+    ros::Rate r(20);
 
     //TEST SECTOR
     //------------------------------------------------
@@ -98,20 +106,55 @@ bool CE_OO::run(){
         pose.pose.position.y = pz * scale_trans_z;
         pose.pose.position.z = px * -scale_trans_x;
         
-
         //scale the rotation
+      
+
         scaled_ox = oy * -scale_rot_y;
         scaled_oy = oz * scale_rot_z;
-        //scaled_oz = ox * scale_rot_x;
-        scaled_oz = oz;
-        
-        
+        scaled_oz = ox * -scale_rot_x;
+/*
+        scaled_ox = ox * scale_rot_x;
+        scaled_oy = oy * scale_rot_y;
+        scaled_oz = oz * scale_rot_z;
 
+        
+        std::cout<< "scaled ox is "<< scaled_ox<<std::endl;
+        std::cout<< "scaled oy is "<< scaled_oy<<std::endl;
+        std::cout<< "scaled oz is "<< scaled_oz<<std::endl;
+ */
+        //Transformation
+        rollMatrix.setEulerYPR(0, 0, scaled_ox);
+        pitchMatrix.setEulerYPR(0, scaled_oy, 0);
+        yawMatrix.setEulerYPR(scaled_oz, 0, 0);
+
+        rotMatrix *= rollMatrix;
+        rotMatrix = pitchMatrix * rotMatrix;
+        rotMatrix = yawMatrix * rotMatrix;
+
+        rotMatrix.getRotation(q);
+ 
+ 
+ 
+ /*       
         //convert Euler to Quaternion
         q = tf::createQuaternionFromRPY(scaled_ox, scaled_oy, scaled_oz);
+        //std::cout<< "x of quaternion q_raw is "<< q_raw.x()<<std::endl;
 
-        //set variables of rotation
-        
+/*
+        rotMatrix.setRPY(ox,oy,oz);
+        //rotMatrix.setEulerYPR(scaled_oz, scaled_oy, scaled_ox);
+        euler_x_omega_new = -euler_y_omega * scale_rot_y;
+        euler_y_omega_new = euler_z_omega * scale_rot_z;
+        euler_z_omega_new = -euler_x_omega * scale_rot_x;
+        std::cout<< "euler_x_omega_new is" << euler_x_omega_new<< std::endl;
+        rotMatrix.setEulerYPR(euler_z_omega_new, euler_y_omega_new, euler_x_omega_new);
+        rotMatrix.getRotation(q);
+        std::cout<< "x of quaternion is q"<< q.x()<<std::endl;
+*/
+
+
+
+        //set rotation variables of PoseStamped message
         pose.pose.orientation.x = q.x();
         pose.pose.orientation.y = q.y();
         pose.pose.orientation.z = q.z();
@@ -145,7 +188,7 @@ int main( int argc, char** argv )
     ros::init(argc, argv, "omega7_qt");
     ros::NodeHandle n;
     CE_OO instance(&n);
-    instance.setScalingFactors(3, 3, 3, 0.03, 0.03, 0.03);
+    instance.setScalingFactors(3, 3, 3, 0.1, 0.03, 0.03);
     instance.run();
 }
 
